407b48a22910822b56337454af9a396c92094bbb
typhonrt <support@typhonrt.org>
2016-01-06 23:25:37 -0800

    update to Parse 1.6.14

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /**
- * Provides the main entry point to the web app first invoking `parseinit` which is a component mapped via
- * `package.json->jspm->dependencies` to `typhonjs-core-parse-init`. `parseinit` will initialize the Parse API by
+ * Provides the main entry point to the web app first invoking `typhonjs-core-parse-init` which is a component mapped
+ * via `package.json->jspm->dependencies`. `typhonjs-core-parse-init` will initialize the Parse API by
  * loading `production-config.js` which is mapped to `parseconfig` in `config-app-paths.js`. Please see `indexSrc.html`
  * and notice that `config-app-paths.js` is loaded after `config.js` is loaded.
  */
 
 'use strict';
 
-import              'parseinit';
-import App     from 'pathSite/js/App.js';
+import      'typhonjs-core-parse-init';
+import App  from 'pathSite/js/App.js';
 
 // Uncomment to turn off eventbus logging.
 // import logger  from 'typhonjs-core-logging';
